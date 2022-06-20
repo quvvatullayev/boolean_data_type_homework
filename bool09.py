d3c34a32538fc8616545a45987662bce930fe5fe
tajriba <quvvatullayev@gmail.com>
2022-06-20 17:49:28 +0300

    bool09.py

--- bool09.py
+++ bool09.py
@@ -1,6 +1,3 @@
-from cgi import print_arguments
-
-
 def main(a):
     """Check the natural number.Natural numbers are numbers used in counting.
     Args:
@@ -10,4 +7,4 @@ def main(a):
     """
     # Write your code here
     return a % 2 == 0 and a > 0 
-print(main(1.2))
+print(main(0))
